30bc52ea59bf91f2fa0cb6989aafa3181a262a9b
the-best-developer <im.dylan.gosnell@gmail.com>
2019-08-15 22:06:35 -0400

    made knexfile and added recipe schema migration

---
+++ migrations/20190815211722_recipe-create.js
@@ -0,0 +1,58 @@
+
+exports.up = function(knex) {
+
+    return (
+        knex.schema
+        .createTable('recipes', table => {
+            table.increments();
+            table.string('recipe_name', 256)
+            .notNullable();
+        })
+        .createTable('ingredients', table => {
+            table.increments();
+            table.string('ingredient_name', 256)
+            .notNullable();
+        })
+        .createTable('steps', table => {
+            table.increments();
+            table.string('step')
+        })
+        .createTable('recipe_ingredients', table => {
+            table.integer('recipe_id')
+                .notNullable()
+                .unsigned()
+                .references('id')
+                .inTable('recipes')
+            table.integer('ingredient_id')
+                .notNullable()
+                .unsigned()
+                .references('id')
+                .inTable('ingredients')
+            table.primary(['recipe_id', 'ingredient_id']);
+        })
+        .createTable('recipe_steps', table => {
+            table.integer('recipe_id')
+                .notNullable()
+                .unsigned()
+                .references('id')
+                .inTable('recipes')
+            table.integer('step_id')
+                .notNullable()
+                .unsigned()
+                .references('id')
+                .inTable('steps')
+            table.primary(['recipe_id', 'step_id']);
+        })
+    );
+};
+
+exports.down = function(knex) {
+    return (
+        knex.schema
+            .dropTableIfExists('recipe_steps')
+            .dropTableIfExists('recipe_ingredients')
+            .dropTableIfExists('steps')
+            .dropTableIfExists('ingredients')
+            .dropTableIfExists('recipes')
+    )
+}
